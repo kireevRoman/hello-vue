f5c273a08cfb4cde48f7d3f073e705ca8c72579c
Кireev Roman <roman.kireev@letovo.ru>
2022-11-19 20:57:30 +0300

    create post list; directive v-for

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
-  <div>
-    <h1>Простой кликер:</h1>
-    <button type="button" v-on:click="addClick">Это кнопка</button>
-    <h2>На кнопку кликнули {{ clicks }} раз</h2>
-  </div>
+  <main class="main-container">
+    <div class="content">
+
+      <div class="post-list">
+        <div class="post-item" v-for="post in posts" :key="`post-${post.id}`">
+          <img class="post-item__image" src="https://unsplash.it/800/800" alt="Превью статьи">
+          <div class="post-item__title">
+            {{ post.title }}
+          </div>
+          <div class="post-item__body">
+            {{ post.body }}
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
   data() {
-    return{
-      clicks: 0
+    return {
+      posts: [
+        {
+          id: 1,
+          title: 'Заголовок статьи',
+          body: ' Текст статьи в несколько строк.  Текст статьи в несколько строк.  Текст статьи в несколько строк.'
+        },
+        {
+          id: 2,
+          title: 'Заголовок статьи 1',
+          body: ' Текст статьи в несколько строк.  Текст статьи в несколько строк.  Текст статьи в несколько строк.'
+        },
+        {
+          id: 3,
+          title: 'Заголовок статьи 2',
+          body: ' Текст статьи в несколько строк.  Текст статьи в несколько строк.  Текст статьи в несколько строк.'
+        },
+
+      ]
     }
   },
-
-  methods: {
-    addClick() {
-      this.clicks += 1
-    }
-  }
+  methods: {}
 }
 </script>
 
 <style lang="scss" scoped>
+* {
+  font-family: Arial,serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 40px;
+
+  .content {
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  background: #f0f1f5;
+}
+
+.post-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.post-item {
+  display: flex;
+  flex-direction: column;
+  width: 48%;
+  margin-bottom: 40px;
+  background-color: #FFFFFF;
+
+  .post-item__image {
+    height: 200px;
+    object-fit: cover;
+  }
+
+  .post-item__title {
+    font-size: 24px;
+    font-weight: 500;
+    padding: 20px 20px 12px;
+  }
+
+  .post-item__body {
+    padding: 12px 20px 20px;
+    font-size: 16px;
+    line-height: 150%;
+  }
+}
+
 button {
   padding: 12px 24px;
   border: 2px solid blueviolet;
